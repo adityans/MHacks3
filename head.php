c88e9387f9dc4c098c7f3de5717339d11941dd44
Aditya Shankar <adityashankar55@Adityas-MacBook-Pro-2.local>
2014-01-18 10:39:44 -0500

    update

--- head.php
+++ head.php
@@ -7,12 +7,11 @@ $dbname = 'helpMe';
 $dbuser = 'root';
 $dbpass = '';
 
-//mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
+mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
 
-//mysql_select_db($dbname) or die(mysql_error());
+mysql_select_db($dbname) or die(mysql_error());
 
-
-
+session_start();
 
 
 ?>
