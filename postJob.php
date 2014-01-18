565494fdbccce60ff17a60c901d5b99e3d52d24b
Andrew Cho <ancho@Andrews-MacBook-Pro-3.local>
2014-01-18 17:43:36 -0500

    DA BETAA

--- postJob.php
+++ postJob.php
@@ -73,9 +73,7 @@
             <li class="list-item"><a href="/info/contact" >Contact</a></li>
         </ul>
         <div class="copyright">Copyright &copy; 2014 taskwal Inc. All Rights Reserved</div>
-        <div id="social-buttons">
-            <div class="fb-like" data-href="https://www.facebook.com/taskwal" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="false" data-send="false"></div>        
-
+        <div id="social-buttons">       
             <a href="https://twitter.com/taskwal" class="twitter-follow-button" data-show-count="false">Follow @vtaskwal</a>
             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
         </div>
