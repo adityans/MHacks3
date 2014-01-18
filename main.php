d16798242a5ba6004f6f4de3fc50463a42509619
Aditya Shankar <adityashankar55@Adityas-MacBook-Pro-2.local>
2014-01-18 00:03:43 -0500

    update

--- main.php
+++ main.php
@@ -2,11 +2,30 @@
 
 require_once "venmo.php";
 
+
+
+
+
 $token = $_REQUEST['access_token'];
 
 $dev = new Venmo;
-$userData = $dev->getUserData($token);
+$dev->setToken();
+$userData = $dev->getUserData();
 $data = json_decode($userData, true);
+
+$userId = $data['data']['user']['id'];
+
+$idQuery = "SELECT userId WHERE userId = '$userId'";
+
+$idReturn = mysql_query($idQuery);
+
+if(!(mysql_num_rows($idReturn)))
+{
+	echo "<h2>Please Enter Your Email Address to Make an Account</h2>";
+	include_once 'enterEmail.php';
+	die();
+}
+
 
 echo "welcome " . $data['data']['user']['username'];
 
