9c3bcf27efffd0758977fdfec7a933b2206b0a7a
Aditya Shankar <adityashankar55@Adityas-MacBook-Pro-2.local>
2014-01-18 10:30:47 -0500

    update

---
+++ addTask.php
@@ -0,0 +1,16 @@
+<form name = 'taskForm' action = "processTask.php" method = "post" >
+	<span>Task Name</span><input name = 'taskName' type = 'text'><br />
+	<span>Description/Details</span><input name = 'taskDesc' type = 'text'><br />
+	<span>When Do You Need it by?<span><input name = 'taskTime' type = 'datetime'><br />
+	<span>Where Do you need it?</span><input name = 'taskWhere' type = 'text'><br />
+	<input type = 'submit' value = 'Create Task'>
+
+</form>
+
+
+<?php
+
+
+
+
+?>
--- head.php
+++ head.php
@@ -12,7 +12,7 @@ mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
 mysql_select_db($dbname) or die(mysql_error());
 
 
-session_start();
+
 
 
 ?>
