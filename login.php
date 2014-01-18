a4ca4dbdc73edf8a651e78aa3dcbac373b1a76b6
unknown <karnvish@umich.edu>
2014-01-18 10:04:24 -0500

    added listings forms

--- login.php
+++ login.php
@@ -2,11 +2,11 @@
 
 require_once 'head.php';
 
-if(isset($_SESSION['userId']))
-{
-	header('Location: listings.php');
-	die();
-}
+//if(isset($_SESSION['userId']))
+//{
+//	header('Location: listings.php');
+//	die();
+//}
 
 
 header("Location: https://api.venmo.com/v1/oauth/authorize?client_id=1552&scope=make_payments%20access_profile");
