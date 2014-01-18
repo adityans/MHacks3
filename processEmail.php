13a86627cb8f7799a96f616788ce4f17a257b1ca
Aditya Shankar <adityashankar55@Adityas-MacBook-Pro-2.local>
2014-01-17 23:42:17 -0500

    update

--- processEmail.php
+++ processEmail.php
@@ -18,10 +18,14 @@ $dev = new Venmo;
 $userData = $dev->getUserData();
 $userInfo = $dev->accessData($userData);
 
+$firstName = $userInfo['data']['user']['first_name'];
+$lastName = $userInfo['data']['user']['last_name'];
+$userId = $userInfo['data']['user']['id'];
 
+$addQuery = "INSERT INTO helpMe VALUES ('$school', '$email', '$firstName', 
+			'$lastName', '$userId'");
 
-
-
+mysql_query($addQuery);
 
 
 ?>
